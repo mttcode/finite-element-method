f7f34d126432549b70bca833f9768045f1162352
Martin Timothy Timko <45509732+mttcode@users.noreply.github.com>
2019-06-05 17:24:22 +0200

    Update f_x.m

--- f_x.m
+++ f_x.m
@@ -1,5 +1,21 @@
+%funkcia pre MKP_1
 function f = f_x(x)
 
 f = -(x-1)*(x^2-x+1);
 
 end
+
+%funkcia pre MKP_2
+%function f = f_x(x)
+
+%f = -x*(x^3+x+2);
+
+%end
+
+
+%funkcia pre MKP_3
+%function f = f_x(x)
+
+%f = sin(x) + exp(-x);
+
+%end
